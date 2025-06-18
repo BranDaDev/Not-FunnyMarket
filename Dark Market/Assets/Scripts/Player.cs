57be7337711f28f1421af60f9974fad4c2576df5
Emyrrhl <145798030+Emyrrhl@users.noreply.github.com>
2025-06-17 21:08:23 -0400

    Additional Scripts (HEIFER)

--- Dark Market/Assets/Scripts/Player.cs
+++ Dark Market/Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
+using Unity.Properties;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private UI_Bag uiBag;
     private Bag bag;
     void Start()
     {
-        
+        uiBag.SetBag(bag);
     }
 
     // Update is called once per frame
---
+++ Dark Market/Assets/Scripts/UI_Bag.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UI_Bag : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        private Bag bag;
+
+    public void SetBag(Bag bag)
+    { 
+        this.bag = bag; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
